846a0483b25662e017530de940d1f022b2574889
James Brown <brownj95@gmail.com>
2021-02-02 16:41:27 +0000

    iterating arrays

--- packing-list.js
+++ packing-list.js
@@ -3,3 +3,9 @@ const packingList = ['tent', 'bug spray', 'sleeping bag'];
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
 }
+
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
